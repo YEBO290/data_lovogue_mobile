721a996aa337f57fd4d8597fa6c76fd918014201
YEBO290 <yebo_sz02@163.com>
2019-04-30 22:25:22 +0800

    xs

--- src/components/menu/index.vue
+++ src/components/menu/index.vue
@@ -2,7 +2,21 @@
   <div class="menu_home">
     <div class="menu_index">  
       <img src="../../assets/image/menulogo.png" style="width: 0.6rem;height: 0.54rem;margin: 0 auto;display: inherit;margin-bottom: 0.36rem;"/>
-      <p class="menu_title login" @click="toLogin"><span class="glyphicon glyphicon-user" style="margin-right:0.12rem;"></span>登录</p>   
+      <el-row style="margin-bottom:20px;">
+        <el-col :span="3" :class="{'userImg': showLogin}">
+          <div class="grid-content bg-purple-dark"><span class="glyphicon glyphicon-user" style="margin-right:0.12rem;"></span></div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple-dark">
+            <p style="font-size: 15px;font-weight: bold;">{{user}}</p>
+            <p @click="toLogin" style="font-size: 12px;text-decoration: underline;color:#666;" v-if="showLogin">切换账号</p>
+          </div>
+        </el-col>
+      </el-row>
+      <!--<p class="menu_title login" @click="toLogin">
+        <span class="glyphicon glyphicon-user" style="margin-right:0.12rem;">{{user}}</span>
+        <span>切换账号</span>
+      </p>  --> 
       <i class="el-icon-close" @click="hideMenu" style="top:0.1rem;"></i>
       <div style="background:#EFDED1;height:0.01rem;margin-bottom:0.25rem;width:100%;"></div>
       <p class="menu_title login" @click="toHome">首页</p>  
@@ -23,10 +37,14 @@
 
 <script>
 import { mapState } from 'vuex'
+import workspace from '../../common.js'
+
 export default {
   data() {
     return {
-      expand: []
+      expand: [],
+      user: workspace.getCookie().name || '登录',
+      showLogin: workspace.getCookie().name ? true: false,
     }
   },
   computed: mapState({
@@ -51,6 +69,9 @@ export default {
       this.$router.push('/home')
     },
     toLogin() {
+      debugger
+      workspace.clearCookie()
+      console.log(workspace.getCookie().name)
       this.$router.push('/login')
     },
     toMenu(val) {
@@ -66,5 +87,9 @@ export default {
 </script>
 
 <style scoped>
+
 @import "./css/index.less";
+.userImg{
+  margin-top: 0.1rem;
+}
 </style>
